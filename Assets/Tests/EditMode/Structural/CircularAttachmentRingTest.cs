1570eb41b04551379b1df8133fc718558351f479
Reuben John <reubenvjohn@gmail.com>
2020-11-07 22:56:58 +0530

    Unit test for CircularAttachmentRing

---
+++ Assets/Tests/EditMode/Structural/CircularAttachmentRingTest.cs
@@ -0,0 +1,24 @@
+﻿using DefaultNamespace;
+using NUnit.Framework;
+using Structural;
+using UnityEngine;
+
+namespace Tests.EditMode.Structural
+{
+    public class CircularAttachmentRingTest
+    {
+        [Test]
+        public void NewTestScriptSimplePasses()
+        {
+            var membraneObj = (GameObject) Resources.Load("Organelles/Membrane1");
+            var circularAttachmentRing = new CircularAttachmentRing();
+
+            circularAttachmentRing.AttachAt(new CircularAttachment(membraneObj.transform, 0f, 15f));
+            Assert.AreEqual(new Vector3(0, .5f, 0), membraneObj.transform.position);
+
+            circularAttachmentRing.AttachAt(new CircularAttachment(membraneObj.transform, 90f, 15f));
+            // ReSharper disable once Unity.InefficientPropertyAccess
+            Assert.AreEqual(new Vector3(-.5f, 0, 0).ToString(), membraneObj.transform.position.ToString());
+        }
+    }
+}
