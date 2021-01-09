9c12484856f93144f679134e19ef3501d5b6f5dc
Reuben John <reubenvjohn@gmail.com>
2021-01-09 19:44:32 +0530

    Blob gizmo for divine recycling timer.
    Took 27 minutes
    
    Took 44 seconds

--- Assets/Scripts/Environment/DivineRecycler.cs
+++ Assets/Scripts/Environment/DivineRecycler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using Chemistry;
 using ChemistryMicro;
 using UnityEngine;
@@ -11,9 +12,13 @@ namespace Environment
         private static readonly Mixture<Substance> UnitFatMix = new MixtureDictionary<Substance> {{Substance.Fat, 1}}
             .ToMixture();
 
+        private static readonly Vector3 Left = Vector3.left * .25f + Vector3.down * .01f;
+        private static readonly Vector3 Right = Vector3.right * .25f + Vector3.down * .01f;
+
         private DivineRecycling divineRecycling;
 
         private ChemicalBlob flask;
+        private float waitStart;
 
         private void Start()
         {
@@ -25,10 +30,29 @@ namespace Environment
 
         private void OnDestroy() => StopAllCoroutines();
 
+        private void OnDrawGizmosSelected()
+        {
+            try
+            {
+                var pos = transform.position;
+                var start = pos + Left;
+                var end = pos + Right;
+                Gizmos.color = Mathf.Approximately(flask[Substance.Fat], flask.TotalMass)
+                    ? Color.white
+                    : Color.green;
+                Gizmos.DrawLine(start, end);
+                Gizmos.color = Color.magenta;
+                Gizmos.DrawLine(start,
+                    Vector3.Lerp(start, end, (Time.time - waitStart) / divineRecycling.divineRecycleInterval));
+            }
+            catch (NullReferenceException) { }
+        }
+
         private IEnumerator StartRecycling()
         {
             while (true)
             {
+                waitStart = Time.time;
                 yield return new WaitForSeconds(divineRecycling.divineRecycleInterval);
                 if (flask != null && !Mathf.Approximately(flask[Substance.Fat], flask.TotalMass))
                 {
