eb3eb0eb39c6f5292eb52ea1d0512ce2cd3af289
Reuben John <reubenvjohn@gmail.com>
2022-04-02 19:14:25 -0400

    Dedicated brain and genes for sheep (distinct from hunters) + minor performance optimizations.
    Took 1 hour 6 minutes

--- Assets/Scripts/Brains/HunterBrain/SheepBrain.cs
+++ Assets/Scripts/Brains/HunterBrain/SheepBrain.cs
@@ -8,15 +8,16 @@ using Util;
 
 namespace Brains.HunterBrain
 {
-    public class HunterBrain : AbstractBrain, ILivingComponent<HunterBrainGene>
+    public class SheepBrain : AbstractBrain, ILivingComponent<HunterBrainGene>
     {
-        public const string ResourcePath = "Organelles/HunterBrain";
+        public const string ResourcePath = "Organelles/SheepBrain";
 
         private LayerMask proximityLayerMask;
 
         private GenealogyGraphManager graphManager;
         private HunterBrainGene gene;
         private Cell.Cell cell;
+
         private Vector2 cellPos;
         private Collider2D[] collidersInRange;
 
@@ -70,22 +71,11 @@ namespace Brains.HunterBrain
 
         protected override void React()
         {
-            if (IsHomeBase()) ReactLikeHomeBase();
-            else ReactLikeHunter();
-        }
-
-        private void ReactLikeHomeBase()
-        {
-            actuatorLogits[SimParams.Singleton.birthCanalIndex][0] = 1f; // Birth
-        }
-
-
-        private void ReactLikeHunter()
-        {
             var cellTransform = cell.transform;
             cellPos = cellTransform.position;
             var nDetected = Physics2D.OverlapCircleNonAlloc(cellPos,
-                cellTransform.localScale.magnitude * SimParams.Singleton.hunterVisibilityRangeRatio, collidersInRange,
+                cellTransform.localScale.magnitude * SimParams.Singleton.sheepVisibilityRangeRatio,
+                collidersInRange,
                 proximityLayerMask.value);
 
             if (nDetected <= 0)
@@ -101,36 +91,20 @@ namespace Brains.HunterBrain
                 return;
 
             var otherCell = closestCollider.GetComponentInParent<Cell.Cell>();
-            if (otherCell.GenealogyNode.Guid == SimParams.Singleton.hunterBaseAGuid)
-            {
-                // Debug.Log("Home base A found");
-            }
-            else if (otherCell.GenealogyNode.Guid == SimParams.Singleton.hunterBaseBGuid)
-            {
-                // Debug.Log("Home base B found");
-            }
-            else
-            {
-                var reproductionGuid =
-                    graphManager.genealogyGraph.GetRelationsTo(otherCell.GenealogyNode.Guid)[0]
-                        .From.Guid;
-                var asexualParentGuid = graphManager.genealogyGraph.GetRelationsTo(reproductionGuid)[0]
+            var reproductionGuid =
+                graphManager.genealogyGraph.GetRelationsTo(otherCell.GenealogyNode.Guid)[0]
                     .From.Guid;
-                if (asexualParentGuid == SimParams.Singleton.hunterBaseAGuid)
-                {
-                    // Debug.Log("Hunter A found");
-                }
-                else if (asexualParentGuid == SimParams.Singleton.hunterBaseBGuid)
-                {
-                    // Debug.Log("Hunter B found");
-                }
+            var asexualParentGuid = graphManager.genealogyGraph.GetRelationsTo(reproductionGuid)[0]
+                .From.Guid;
+            if (asexualParentGuid == SimParams.Singleton.hunterBaseAGuid
+                || asexualParentGuid == SimParams.Singleton.hunterBaseBGuid)
+            {
+                // Debug.Log("Hunter found");
             }
 
             actuatorLogits[SimParams.Singleton.flagellaIndex][0] = 1f; // Force
-            actuatorLogits[SimParams.Singleton.flagellaIndex][1] = -0.03f; // Torque
-            actuatorLogits[SimParams.Singleton.orificeIndex][0] = 1f; // Eat
+            actuatorLogits[SimParams.Singleton.flagellaIndex][1] = 1f; // Torque
         }
-
 
         private float DistanceToCollider(Collider2D otherCollider)
         {
@@ -138,9 +112,5 @@ namespace Brains.HunterBrain
             var distance = (closestPoint - cellPos).magnitude;
             return distance == 0 ? float.MaxValue : distance;
         }
-
-        private bool IsHomeBase() =>
-            cell.GenealogyNode.Guid == SimParams.Singleton.hunterBaseAGuid ||
-            cell.GenealogyNode.Guid == SimParams.Singleton.hunterBaseBGuid;
     }
 }
--- Assets/Scripts/Environment/SimParams.cs
+++ Assets/Scripts/Environment/SimParams.cs
@@ -1,16 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Environment
 {
     public class SimParams : MonoBehaviour
     {
-        public string hunterBaseAGuid = "7f146de4-9367-4bf8-bef0-82b08b5c10c6";
-        public string hunterBaseBGuid = "7f146de4-9367-4bf8-bef0-82b08b5c10c7";
+        public string hunterBaseAGuidString = "7f146de4-9367-4bf8-bef0-82b08b5c10c6";
+        public string hunterBaseBGuidString = "7f146de4-9367-4bf8-bef0-82b08b5c10c7";
+
+        public Guid hunterBaseAGuid => Guid.Parse(hunterBaseAGuidString);
+        public Guid hunterBaseBGuid => Guid.Parse(hunterBaseBGuidString);
 
         public int flagellaIndex = 1;
         public int birthCanalIndex = 2;
         public int orificeIndex = 3;
-        public float cellVisibilityRangeRatio = 5f;
+        public float hunterVisibilityRangeRatio = 5f;
+        public float sheepVisibilityRangeRatio = 3f;
 
         public LayerMask cellLayerMask;
         public LayerMask chemicalBlobLayerMask;
