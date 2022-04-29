5717ea9146f83634d7b931f8ae09b6c8439fe932
Reuben John <reubenvjohn@gmail.com>
2022-04-29 17:26:14 -0400

    Bug fix in cohesion force
    Took 49 minutes

--- Assets/Scripts/Brains/HunterBrain/HunterBrain.cs
+++ Assets/Scripts/Brains/HunterBrain/HunterBrain.cs
@@ -174,18 +174,15 @@ namespace Brains.HunterBrain
             return WanderTarget();
         }
 
-        private Vector2 CohesionTarget(Vector2 WanderTargetPostion)
+        private Vector2 CohesionForce()
         {
             Vector2 basePosition = friendlyBase.transform.position;
-            float cellMass = cellCauldron.TotalMass;
-            float baseMass = friendlyBase.cellCauldron.TotalMass;
-            float angleToBase = Mathf.Atan2(basePosition.y - cellPos.y, basePosition.x - cellPos.x);
-            Vector2 baseVector = cellPos + new Vector2(Mathf.Cos(angleToBase), Mathf.Sin(angleToBase));
-            var targetPosition = WanderTargetPostion + baseVector * (cellMass / baseMass) * SimParams.Singleton.cohesionCoeff;
-            Debug.DrawLine(cellPos, WanderTargetPostion, Color.green);
-            // Debug.DrawLine(cellPos, basePosition, Color.yellow);
-            Debug.DrawLine(cellPos, baseVector, Color.magenta);
-            return targetPosition;
+            var angleToBase = Mathf.Atan2(basePosition.y - cellPos.y, basePosition.x - cellPos.x);
+            var cohesionStrength = cellCauldron.TotalMass / friendlyBase.cellCauldron.TotalMass *
+                                   SimParams.Singleton.cohesionCoeff;
+            var cohesionForce = cohesionStrength * new Vector2(Mathf.Cos(angleToBase), Mathf.Sin(angleToBase));
+            Debug.DrawLine(cellPos, cellPos + cohesionForce, Color.yellow);
+            return cohesionForce;
         }
 
         private Vector2 WanderTarget()
@@ -203,7 +200,7 @@ namespace Brains.HunterBrain
             var rangeProbe = -100f + wanderSeed + SimParams.Singleton.wanderRangeFluctuation * Time.time;
             var targetPosition = cellPos + new Vector2(Mathf.Sin(wanderAngle), Mathf.Cos(wanderAngle)) *
                 (0.5f + Mathf.PerlinNoise(rangeProbe, rangeProbe)) * SimParams.Singleton.wanderRange;
-            return CohesionTarget(targetPosition);
+            return targetPosition + CohesionForce();
         }
 
         private void ReactLikeHunter()
